79a60571055460b54f4e1e171b5b541ac4f50b39
YourName <youremail@example.com>
2026-05-24 12:14:33 +0300

    Fixed admin URL routing and added applications URLs

---
+++ applications/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Add application-related URLs here in the future
+]
--- ncpb_portal/urls.py
+++ ncpb_portal/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('', include('accounts.urls')),
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('applications/', include('applications.urls')),
 ]
